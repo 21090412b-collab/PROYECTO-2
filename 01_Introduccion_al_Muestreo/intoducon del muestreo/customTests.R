df5f2281f65ebe8c0633e0fe703bb9b2b5c679c9
21090412b-collab <21090412b@gmail.com>
2026-09-08 19:17:57 -0600

    Delete 01_Introduccion_al_Muestreo/intoducon del muestreo directory

--- 01_Introduccion_al_Muestreo/intoducon del muestreo/customTests.R
+++
@@ -1,6 +0,0 @@
-# customTests.R - 01_Introduccion_al_Muestreo
-#
-# Esta leccion no requiere funciones de verificacion personalizadas: todas
-# sus preguntas se validan con las pruebas estandar de swirl (omnitest).
-# Se conserva este archivo, generado por swirlify, por consistencia de
-# estructura entre lecciones.
